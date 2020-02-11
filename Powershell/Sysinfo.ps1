f4a7636b5c3316dd2e80cb4c100dbff008854c8c
pwill1998 <35737694+pwill1998@users.noreply.github.com>
2020-02-11 14:09:02 -0500

    Add files via upload

---
+++ Powershell/Sysinfo.ps1
@@ -0,0 +1,6 @@
+﻿function getIP{(Get-NetIPAddress).IPv4Address|Select-String "192*"}
+write-host(getIP)
+$ip=getip 
+write-host("this machines's IP is $ip")
+Write-Host("this machines ip {0}" -f $ip)
+send-mailmessage -to "willispd@mail.uc.edu" "IT3038 Windows SysInfo" -Body $Body -smtserver stmtp.gmail.com -port 587 -usessl -credential(Get-Credential)
